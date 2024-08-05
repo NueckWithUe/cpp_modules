e89b3cc244c63e6c574137ff0457d9b5a8f3a9fe
Nils Nagel <nnagel@student.42heilbronn.de>
2024-08-05 13:48:19 +0200

    feat: extended the Contact class

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -9,3 +9,46 @@ Contact::~Contact()
 {
 	std::cout << "Contact destructor called" << std::endl;
 }
+
+std::string Contact::get_first_name()
+{
+	return (first_name);
+}
+std::string Contact::get_last_name()
+{
+	return (last_name);
+}
+std::string Contact::get_nickname()
+{
+	return (nickname);
+}
+int Contact::get_phone_number()
+{
+	return (phone_number);
+}
+std::string Contact::get_secret()
+{
+	return (secret);
+}
+
+
+void Contact::set_first_name(std::string input)
+{
+	first_name = input;
+}
+void Contact::set_last_name(std::string input)
+{
+	last_name = input;
+}
+void Contact::set_nickname(std::string input)
+{
+	nickname = input;
+}
+void Contact::set_phone_number(int input)
+{
+	phone_number = input;
+}
+void Contact::set_secret(std::string input)
+{
+	secret = input;
+}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -9,10 +9,27 @@
 class Contact
 {
 private:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	int			phone_number;
+	std::string	secret;
 
 public:
 	Contact();
 	~Contact();
+
+	std::string	get_first_name();
+	std::string	get_last_name();
+	std::string	get_nickname();
+	int			get_phone_number();
+	std::string	get_secret();
+
+	void set_first_name(std::string input);
+	void set_last_name(std::string input);
+	void set_nickname(std::string input);
+	void set_phone_number(int input);
+	void set_secret(std::string input);
 };
 
 #endif
