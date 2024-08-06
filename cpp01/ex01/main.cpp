a0c79f07a9d21324192dbd3ec7ccdf54c1ee2639
Nueck <nils1.nagel9@gmail.com>
2024-08-06 19:23:58 +0200

    feat: added test

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -2,5 +2,17 @@
 
 int	main(void)
 {
+	int amount;
+	int count;
+
+	count = 1;
+	amount = 5;
+	Zombie *horde = zombieHorde(amount, "Zombie");	
+	for (int i = 0; i < amount; i++)
+	{
+		std::cout << count++ << ". Zombie" << std::endl;
+		horde[i].announce();
+	}
+	delete []horde;
 	return (0);
 }
