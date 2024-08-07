a331010ef2e96977aa6fe8cf782416cfd725dfc0
Nils Nagel <nnagel@student.42heilbronn.de>
2024-08-07 08:47:33 +0200

    fix: changed output to not concatenate strings anymore

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -11,7 +11,7 @@ HumanB::~HumanB()
 
 void	HumanB::attack()
 {
-	std::cout << name + " attacks with their " + weapon.getType() << std::endl;
+	std::cout << name << " attacks with their " << weapon.getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon newWeapon)
