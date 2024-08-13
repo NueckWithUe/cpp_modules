22843f45fc3962492010e4603995c7c7057113e3
Nils Nagel <nnagel@student.42heilbronn.de>
2024-08-13 14:53:27 +0200

    fix: copies the content of <file> into <file>.replace

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,10 +5,12 @@ int	main(int argc, char **argv)
 {
 	std::ifstream inputfile;
 	std::string lines;
+	std::string name = argv[1];
+	name = name.append(".replace");
 
 	if (argc != 4)
 	{
-		std::cout << "Error.\nWrong Arguments.\nArguments must be: <filename> <s1> <s2>" << std::endl;
+		std::cout << "Error.\nWrong Arguments.\nArguments must be: <filename> <string_to_find> <string_to_replace>" << std::endl;
 		return (1);
 	}
 	inputfile.open(argv[1], std::ifstream::in);
@@ -17,8 +19,14 @@ int	main(int argc, char **argv)
 		std::cout << "Error.\nFile cannot be opened." << std::endl;
 		return (1);
 	}
+	std::ofstream outfile (name);
 	while (std::getline(inputfile, lines))
+	{
 		std::cout << lines << std::endl;
+		if ((int)lines.find(argv[2], 0) >= 0)
+			std::cout << argv[2] << " found." << std::endl;
+		outfile << lines << std::endl;
+	}
 	inputfile.close();
 	return (0);
 }
