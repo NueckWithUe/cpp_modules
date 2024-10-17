a36f8e87e0d79139e86e82147001728d6cd2b6f8
Nils Nagel <nnagel@student.42heilbronn.de>
2024-10-17 16:09:12 +0200

    fix: changed approach on how to create the replaced string

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <fstream>
 
-void replace(std::string &line, char **argv)
+void replace(std::string &line, char **argv, int pos)
 {
-	int found = (int)line.find(argv[2], 0);
-	char s[1024];
+	int found = (int)line.find(argv[2], pos);
+	std::string start = line.substr(pos, found);
+	std::string to_find = argv[2];
+	std::string replace = argv[3];
+	std::string finish = line.substr(found + to_find.length(), replace.length() + start.length());
 	std::string new_line;
-	std::string argv2 = argv[2];
-	std::string argv3 = argv[3];
 
-	line.copy(s, found, 0);
-	new_line.append(s);
-	new_line.append(argv3);
-	new_line.append(line, found + argv2.length(), 1024);
+	new_line.append(start);
+	new_line.append(replace);
+	new_line.append(finish);
+	// char s[1024];
+	// std::string new_line;
+	// std::string argv2 = argv[2];
+	// std::string argv3 = argv[3];
+
+	// line.copy(s, found, pos);
+	// new_line.append(s);
+	// new_line.append(argv3);
+	// new_line.append(line, found + argv2.length(), 1024);
 	line = new_line;
 }
 
 int	main(int argc, char **argv)
 {
-	std::ifstream inputfile;
+	std::ifstream inputfile(argv[1]);
 	std::string lines;
+	std::string next_line;
 	std::string name = argv[1];
+	std::string to_find = argv[2];
+	std::string to_replace = argv[3];
+	int curr_pos = 0;
 	name = name.append(".replace");
 
 	if (argc != 4)
@@ -28,7 +41,6 @@ int	main(int argc, char **argv)
 		std::cout << "Error.\nWrong Arguments.\nArguments must be: <filename> <string_to_find> <string_to_replace>" << std::endl;
 		return (1);
 	}
-	inputfile.open(argv[1], std::ifstream::in);
 	if (!inputfile.is_open())
 	{
 		std::cout << "Error.\nFile cannot be opened." << std::endl;
@@ -37,9 +49,16 @@ int	main(int argc, char **argv)
 	std::ofstream outfile(name);
 	while (std::getline(inputfile, lines))
 	{
-		while ((int)lines.find(argv[2], 0) >= 0)
-			replace(lines, argv);
-		outfile << lines << std::endl;
+		while ((int)lines.find(argv[2], curr_pos) >= 0 && to_find != to_replace)
+		{
+			replace(lines, argv, curr_pos);
+			curr_pos += (to_replace.length() + to_find.length());
+		}
+		curr_pos = 0;
+		outfile << lines;
+		if (!inputfile.eof())
+			outfile << std::endl;
+		lines.clear();
 	}
 	inputfile.close();
 	outfile.close();
