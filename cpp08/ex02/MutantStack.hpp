9e547656dc90637641942728536f76fd31839730
Nils <nilsnagel04@gmail.com>
2025-01-13 19:49:02 +0100

    fix: completed mutantstack class and provided some tests

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -5,9 +5,22 @@
 #include <list>
 #include <stack>
 
-template <typename T> class MutantStack : public std::stack<T>, public std::iterator<T>
+template <typename T, class container=std::deque<T>> class MutantStack : public std::stack<T>
 {
 public:
 	MutantStack() {}
+	MutantStack(const MutantStack& obj) {*this = obj;}
+	MutantStack& operator=(const MutantStack& obj) {return *this;}
 	~MutantStack() {}
+
+	typedef typename container::iterator iterator;
+
+	iterator begin()
+	{
+		return (this->c.begin());
+	}
+	iterator end()
+	{
+		return (this->c.end());
+	}
 };
